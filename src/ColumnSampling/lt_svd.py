8a0d08eb2ad16e493cdfc5abd82851d5e5284cd5
Amey <amey.desai@utah.edu>
2014-03-09 16:39:22 -0600

    Constant SVD start

--- src/ColumnSampling/lt_svd.py
+++ src/ColumnSampling/lt_svd.py
@@ -17,13 +17,20 @@ nf : Norm Factor 1 for Spectral, K for Frob
 
 TODO : Check if the value returned is greater than col count
 '''
-def setEta(beta, delta):
-    return 1 + np.sqrt((8/beta) * np.log(1/delta))
-
+def setEta(beta, delta, algo):
+    if algo == 1:
+        return 1 + np.sqrt((8/beta) * np.log(1/delta))
+    else:
+        return 1 + np.sqrt(8 * np.log(2/delta))
 def setNoOfCols(eps, nf, eta, beta):
     #c = nf/ (eps ** 2)
     c = (4 * eta ** 2 )/ (beta * eps **2)
     return int(round(c))
+
+def setNoofSamples(k, eta, eps):
+    print k, eta, eps
+    s = (k ** 2 * eta **2) / (eps ** 4)
+    return s
 
 def getLeftSingVec(sampled_mat, k):
     V, s, Vt = np.linalg.svd(np.dot(sampled_mat.transpose(), sampled_mat))
@@ -45,6 +52,14 @@ def validate(imat, lsmat, k):
     fn = np.linalg.norm(imat, 'fro') ** 2
     print (lhs - rhs)/fn
 
+def constantTimeSVD(eta, k, rn_mat, eps):
+    s = setNoofSamples(k, eta, eps)
+    print s
+    #sampled_mat = sampleCols(rn_mat, s)
+    #den = np.linalg.norm(sampled_mat, 'fro') ** 2
+    #row_prob = [(np.linalg.norm(col, 2) ** 2)/den for col in sampled_mat ]
+    #print row_prob
+    #print sampled_mat.shape
 def main():
     parser = argparse.ArgumentParser(description = 'Read Matrix Data')
     parser.add_argument('dataFile', nargs = '+', help = 'Enter complete path of Data File')
@@ -52,14 +67,17 @@ def main():
     parser.add_argument('beta', help = 'Enter value for beta')
     parser.add_argument('delta', help = 'Enter value for delta')
     args = parser.parse_args()
-    eta = setEta(float(args.beta), float(args.delta))
-    rn_mat = generateData(40, 1000)
+    eta = setEta(float(args.beta), float(args.delta), 2)
+    
+    rn_mat = generateData(100, 20000)
     rank_mat = rank(rn_mat)
-    c = setNoOfCols(float(args.eps), 1, eta, float(args.beta))
+    
+    constantTimeSVD(eta, rank_mat/5, rn_mat, float(args.eps))
+    #c = setNoOfCols(float(args.eps), 1, eta, float(args.beta))
     #print c, eta
-    sampled_mat = sampleCols(rn_mat, c)
-    lsv = getLeftSingVec(sampled_mat, rank_mat/2)
-    validate(rn_mat, lsv, rank_mat/2)
+    #sampled_mat = sampleCols(rn_mat, c)
+    #lsv = getLeftSingVec(sampled_mat, rank_mat/2)
+    #validate(rn_mat, lsv, rank_mat/2)
 if __name__ == '__main__':
     try:
         main()
